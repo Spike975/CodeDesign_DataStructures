bb525d4f2665cd280b6787871e0b79be5700d870
Jacob Firsdon <firsdonj@gmail.com>
2020-01-22 12:49:15 -0800

    Starting Linked Lists again

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -1,13 +1,13 @@
 #include "tLinkedList.h"
 
 int main() {
-	tForwardList<int> test;
-	test.push_front(4);
-	test.push_front(5);
-	test.push_front(12);
-	test.push_front(9);
-	test.pop_front();
-	test.pop_front();
-	std::cout << test.front();
+	tForwardList<int> * test = new tForwardList<int>;
+	test->push_front(4);
+	test->push_front(5);
+	test->push_front(12);
+	test->push_front(9);
+	test->pop_front();
+	test->pop_front();
+	std::cout << test->front();
 	return 0;
 }
